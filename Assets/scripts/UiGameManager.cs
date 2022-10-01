858aee95608643658676e307c1b4d6be5ffa111b
Seaney <sfs185@humboldt.edu>
2022-10-01 16:10:32 -0700

    update UI manager

--- Assets/scripts/UiGameManager.cs
+++ Assets/scripts/UiGameManager.cs
@@ -5,51 +5,95 @@ using UnityEngine.SceneManagement;
 using TMPro;
 public class UiGameManager : MonoBehaviour
 {
+    /// <summary>
+    /// the scenes to go through in order DO NOT UPDATE OUTSIDE OF PREFAB
+    /// </summary>
+    private static string[] scenes = {"example"};
 
-    
-    private static string[] scenes;
+    /// <summary>
+    /// Turn on to reload the the DEBUG Scene 
+    /// </summary>
+    public bool DEBUG_MODE;
+
+    /// <summary>
+    /// set to the name of the scene you want to load
+    /// </summary>
+    //public string DEBUG_SCENE;
+
+    // current level
     private static int level;
+    // current lives
     private static int lives;
+
+    // tacks how long we have been in level
     private float levelTimer = 10f;
-    private const int STARTING_LIVES = 6;
+
+    // STARTING values
+    private const int STARTING_LIVES = 3;
     private const int STARTING_LEVEL = 0;
 
-    // this is the worst way to check if a game has ended
+
+
+    /// <summary>
+    /// track what state the game is in
+    /// TODO change to enums
+    /// </summary>
     public bool GameOver = false;
     public static bool Puased = true;
+    
     public GameObject GameOverUi;
 
+
+    // UI handlers see other files
     public TextMeshProUGUI levelCount;
     public lifeCounter lifeHandler;
     public sheepTimer timerHandler;
 
-    public static void startGame()
+
+
+    /// <summary>
+    /// depricated switch over to startGamefromLevel
+    /// </summary>
+    private static void startGame()
     {
         level = STARTING_LEVEL;
         lives = STARTING_LIVES;
         Puased = false; 
-        
-        // TODO set up scenes in code
-        // e.g 
-        // scenes = new string[numberOfScenes]
-        // scenes[i] = "sceneName" 
-        // to use copy above line and replace numberOfScenes with the number of scenes and replicate the line below that for each new scene
     }
 
 
-    
+    /// <summary>
+    /// Use to start game from a scene using scene index, review const scenes variable at top of file to know index
+    /// </summary>
+    /// <param name="levelToStartFrom">index of scene to start at</param>
+    public static void StartGameFromlevel(int levelToStartFrom)
+    {
+        startGame();
+        SceneManager.LoadScene(scenes[levelToStartFrom]);
+    }
+
+    /// <summary>
+    /// Use this to start at a specific scene if you do not know the index of the scene in order
+    /// </summary>
+    /// <param name="levelToStartFrom">Name of scene to load</param>
+    public static void StartGameFromScene(string levelToStartFrom)
+    {
+        startGame();
+        SceneManager.LoadScene(levelToStartFrom);
+    }
+
 
     void Awake()
     {
+        if (DEBUG_MODE)
+        {
+            startGame();
+        }
         //startGame();
         lifeHandler.init(lives);
         timerHandler.init(levelTimer);
-    }
+        levelCount.text = "Level : "+ ((int)(level+1)).ToString();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -66,7 +110,6 @@ public class UiGameManager : MonoBehaviour
         else if (levelTimer > 0)
         {
             levelTimer -= Time.deltaTime;
-            levelCount.text = "Level : "+ ((int)(level+1)).ToString("N");
             timerHandler.TimerActiveUpdate(levelTimer); 
         }
         else
@@ -76,24 +119,48 @@ public class UiGameManager : MonoBehaviour
     }
 
 
-    public void loadNextScene()
+    /// <summary>
+    /// internal use only, will handle loading next scene
+    /// </summary>
+    private void loadNextScene()
     {
+        // while in debug mode reload the same scene over and over level will not tic up properly nor will game over screen occure
+        if (DEBUG_MODE)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+
+        }
+
 
         level++;
         if (lives <= 0)
         {
+            // if zero lives end game
             EndGame();
         }
         else
         {
-            // load level index scene
-            //SceneManager.LoadScene(scenes[level]);
-            SceneManager.LoadScene("example");
+            try
+            {
+                SceneManager.LoadScene(scenes[level]);
+            }
+            catch
+            {
+                // if we do not have any more levels continuasly reload last scene
+                SceneManager.LoadScene(scenes[scenes.Length-1]);
+            }
 
-            // TODO add animation logic
         }
     }
 
+
+    /// <summary>
+    /// call to make game add a life 
+    /// TODO IDK if this should be handled internally lets talk about it more
+    /// </summary>
     public void gainLife()
     {
         lifeHandler.addLife();
@@ -101,17 +168,61 @@ public class UiGameManager : MonoBehaviour
     }
 
     /// returns true if player is still alive
-    // MAKE SURE TO HALT MINI GAME IF ON FALSE
     public void removeLife()
     {
         lifeHandler.removeLife();
         lives--; 
     }
 
+    /// <summary>
+    /// run to check if the game is over
+    /// </summary>
+    /// <returns>true if game is over</returns>
+    public bool isGameOver()
+    {
+        return lives <= 0;
+    }
+
+    /// <summary>
+    /// Call this when the mini game is won
+    /// </summary>
+    public void winMiniGame()
+    {
+        loadNextScene();
+    }
+
+
+    /// <summary>
+    /// if you lose a mini game and want to end it early call this.
+    /// </summary>
+    public void loseMiniGame()
+    {
+        //TBD
+        removeLife();
+        loadNextScene();
+    }
+
+
+    public float GetTimeleftInGame()
+    {
+        return levelTimer;
+    } 
+
     // Call when your mini game has ended if player was killed, will automatically be called on scene load if player has zero lives
     public void EndGame()
     {
         GameOver = true;
         GameOverUi.SetActive(true);
+    }
+
+    [System.Serializable]
+    public class UiGameManagerException : System.Exception
+    {
+        public UiGameManagerException() { }
+        public UiGameManagerException(string message) : base(message) { }
+        public UiGameManagerException(string message, System.Exception inner) : base(message, inner) { }
+        protected UiGameManagerException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
